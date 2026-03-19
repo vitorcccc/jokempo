80554a829fc37fec49b105acc5cee3ac0e7b6aee
vitorcccc <165915401+vitorcccc@users.noreply.github.com>
2026-03-18 22:41:07 -0300

    Adiciona classe ResultadoRodada

---
+++ Jokempo.Core/ResultadoRodada.cs
@@ -0,0 +1,11 @@
+namespace Jokempo.Core
+{
+    public class ResultadoRodada
+    {
+        public string NomeJogador { get; set; }
+        public string NomeJogadaJogador { get; set; }
+        public string NomeJogadaComputador { get; set; }
+        public ResultadoJogo Resultado { get; set; }
+        public string MensagemResultado { get; set; }
+    }
+}
